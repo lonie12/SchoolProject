c03b7c3c0d060d719bb990a5fe0e353c5197e060
akrabal <96004103+KimoyoStack@users.noreply.github.com>
2022-04-27 15:24:47 +0000

    fonctionaliter approvisionement

--- src/main/java/com/fastProject/fastProect/Approvision/ApprovisionController.java
+++ src/main/java/com/fastProject/fastProect/Approvision/ApprovisionController.java
@@ -8,28 +8,40 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.List;
+
 @Controller
 public class ApprovisionController {
     private  final ApprovisionService approvisionService;
+    private  final  ArticleService articleService;
 
-    public ApprovisionController(ApprovisionService approvisionService) {
+    public ApprovisionController(ApprovisionService approvisionService, ArticleService articleService) {
         this.approvisionService = approvisionService;
+        this.articleService = articleService;
     }
 
     @GetMapping("/addApprovision")
     public String addApprovision(Model model){
         model.addAttribute("approvision",new Approvision());
+        List<Article> articles =   articleService.giveAll();
+        model.addAttribute("articles",articles);
         return "Approvision/enr_approvision";
     }
 
     @PostMapping("/addApprovision")
     public  String  PostaddApprovision(Model model,@ModelAttribute("approvision") Approvision approvision){
         approvisionService.addApprovison(approvision);
-        return  "redirect:/GiveApprovision";
+        return  "redirect:/GiveApprovisions";
     }
 
-    @GetMapping("/GiveAppprovision")
-    public String GiveApprovision(){
-        return "";
+    @GetMapping("/GiveApprovisions")
+    public String GiveApprovision(Model model){
+         List<Approvision>  approvisions = approvisionService.getAllApprovision();
+         model.addAttribute("approvisions",approvisions);
+         for (int i = 0; i < approvisions.size(); i++) {
+                System.out.println(approvisions.get(i).getDateApprovision());
+         }
+
+         return "Approvision/list_approvision";
     };
 }
